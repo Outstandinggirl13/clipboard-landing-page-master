986b44d2b0d0b1682c2f53ff465189105e0a6018
Outstandinggirl13 <108367073+Outstandinggirl13@users.noreply.github.com>
2025-11-02 18:40:39 +0200

    Switch to responsive units.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const historyTitleElement = document.querySelector(".history__title");
 const accessTitleElement = document.querySelector(".access__title")
+
+// when the websites loads
 if (window.innerWidth > 750) {
-    historyTitleElement.textContent = "A history of everything you copy";
     accessTitleElement.textContent = "Access Clipboard anywhere";
 }
+
+// when the user resizes window
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 750) {
+        accessTitleElement.textContent = "Access Clipboard anywhere";
+    } else {
+        accessTitleElement.textContent = "Access Clipboard Anywhere";
+    }
+  });
