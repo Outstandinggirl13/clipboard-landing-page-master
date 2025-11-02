f7708273b9a65fabc0c4881eae77f5ef5f0df960
Outstandinggirl13 <108367073+Outstandinggirl13@users.noreply.github.com>
2025-11-02 20:50:27 +0200

    Minor adjustments.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const historyTitleElement = document.querySelector(".history__title");
 const accessTitleElement = document.querySelector(".access__title")
 
-// when the websites loads
+// when the website loads
 if (window.innerWidth > 750) {
     accessTitleElement.textContent = "Access Clipboard anywhere";
 }
 
-// when the user resizes window
+// when the user resizes the window
 window.addEventListener("resize", () => {
     if (window.innerWidth > 750) {
         accessTitleElement.textContent = "Access Clipboard anywhere";
